1e6478dba7578adb45e3344194778be3a7531b1e
sebafudi <sebastianfudalej@gmail.com>
2021-09-27 13:52:26 +0200

    feat: storing state in localStorage

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,13 +1,33 @@
+import { useEffect, useState } from "react";
+
 import "./Item.scss";
 
-function Item({ text }) {
+function Item(props) {
+  const localChecked = JSON.parse(localStorage.getItem(props.text));
+  const [checked, setChecked] = useState(
+    localChecked !== null ? localChecked : props.checked ? true : false
+  );
+
+  function toggleItem() {
+    setChecked(!checked);
+  }
+
+  useEffect(() => {
+    localStorage.setItem(props.text, checked);
+  }, [checked, props.text]);
+
   return (
-    <label htmlFor={text} className="todo-item">
+    <label htmlFor={props.text} className="todo-item">
       <div className="todo-item-checkbox">
-        <input type="checkbox" id={text} />
-        <span class="checkmark"></span>
+        <input
+          type="checkbox"
+          defaultChecked={checked}
+          onChange={toggleItem}
+          id={props.text}
+        />
+        <span className="checkmark"></span>
       </div>
-      <div className="todo-item-text">{text}</div>
+      <div className="todo-item-text">{props.text}</div>
     </label>
   );
 }
--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,12 +1,30 @@
 import Item from "../Item/Item";
 import "./List.scss";
 
+let itemArray = [
+  {
+    text: "test",
+    checked: false,
+  },
+  {
+    text: "test2",
+    checked: true,
+  },
+  {
+    text: "test3",
+    checked: false,
+  },
+  {
+    text: "test1 2 3",
+  },
+];
+
 function List() {
   return (
     <div className="todo-list">
-      <Item text="First task" />
-      <Item text="Second task" />
-      <Item text="Third task" />
+      {itemArray.map((item) => (
+        <Item text={item.text} checked={item.checked} />
+      ))}
     </div>
   );
 }
